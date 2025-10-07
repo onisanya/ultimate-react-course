5a88f16ad1085dead5b661678c492b7c7e094518
onisanya <onisanya@gmail.com>
2025-10-08 01:10:52 +0200

    05-travel-list increments

--- 05-travel-list/05-travel-list/src/App.js
+++ 05-travel-list/05-travel-list/src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function Logo() {
+  return (
+    <div className="app-header">
+      <h1>Far Away</h1>
+    </div>
+  );
+}
+
+function TravelList() {
+  return (
+    <ul className="list">
+      <Item />
+      <Item />
+      <Item />
+    </ul>
+  );
+}
+
+function Form() {
+  const [quantity, setQuantity] = useState(1);
+
+  function reset() {
+    setQuantity(1);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submitting");
+    reset();
+  }
+
+  function modifyQuantity(e, amount) {
+    e.preventDefault();
+    setQuantity((q) => {
+      if (quantity + amount < 1) return 1;
+      else {
+        return quantity + amount;
+      }
+    });
+  }
+
+  return (
+    <form className="add-form">
+      <h3>I will take... </h3>
+      <span> </span>
+      <span className={quantity <= 1 ? "multiplier" : "multiplier__active"}>
+        {quantity} x
+      </span>
+      <input type="text" placeholder="Item description" />
+      <button onClick={(e) => modifyQuantity(e, 1)}>+</button>
+      <button onClick={(e) => modifyQuantity(e, -1)}>-</button>
+      <button onClick={handleSubmit}>Add</button>
+    </form>
+  );
+}
+
+function Item() {
+  return (
+    <li className="item">
+      <input type="checkbox" />
+      <span>India</span>
+      <button className="delete">❌</button>
+    </li>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <TravelList />
+      <Stats />
     </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em> 0 items</em>
+    </footer>
   );
 }
 
