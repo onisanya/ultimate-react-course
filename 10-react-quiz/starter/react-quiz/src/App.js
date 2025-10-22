5e48496f476f7cfbdcc7ee05a20a47be2f1c2e6f
onisanya <onisanya@gmail.com>
2025-10-22 17:50:41 +0200

    Quiz: working start component

--- 10-react-quiz/starter/react-quiz/src/App.js
+++ 10-react-quiz/starter/react-quiz/src/App.js
@@ -1,10 +1,53 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialState = { questions: [], status: "loading" };
+
+function reducer(state, action) {
+  console.log(action);
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
 
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => {
+        console.error("Error", err);
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={questions.length} dispatch={dispatch} />
+        )}
+      </Main>
     </div>
   );
 }
---
+++ 10-react-quiz/starter/react-quiz/src/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img
+        src="https://reactjs.org/logo-og.png"
+        className="app-logo"
+        alt="logo"
+      />
+      <h1>React Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
